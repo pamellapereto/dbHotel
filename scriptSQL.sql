c49e2ab941ff3a68562002e982e0adb038abc055
pamellapereto <pamellapereto@gmail.com>
2023-10-23 11:55:59 -0300

    Quartos cadastrados e criada a tabela clientes

--- scriptSQL.sql
+++ scriptSQL.sql
@@ -3,6 +3,7 @@ show databases;
 /* SELECIONAR O BANCO */
 use dbPamella;
 
+use dbHotel;
 
 /* Excluir a tabela do banco */
 drop table funcionarios;
@@ -119,7 +120,7 @@ create table quartos (
     situacao char(3) not null,
     nome varchar(50) not null,
     descricao text,
-    preco decimal(10,2) not null,
+    preco decimal(10,2) not null,    
     tipoCama varchar(20),
     varanda char(3)
 );
@@ -129,10 +130,52 @@ describe quartos;
 
 alter table quartos add column numeroQuarto varchar(10) not null after andar;
 
+/* ADICIONAR COMANDOS ABAIXO */
+alter table quartos add column cafeDaManha char(3) not null after preco;   
+alter table quartos add column foto varchar(255) not null after descricao;
+
+insert into quartos (andar, numeroQuarto, tipoQuarto, ocupacaoMax, situacao, nome, descricao, preco, tipoCama, varanda) values ("5º", "505", "Superior Premier", 3, "não",
+"Familiar", "O quarto de 32m² com piso frio, varanda - vista para o mar. Oferece ar condicionado individual, TV LCD 42’’, wi-fi grátis, cofre digital, 
+frigobar abastecido e banheiro com secador de cabelo e amenities e mesa de trabalho.", 750.90, "Queen Size", "sim");
+
+update quartos set cafeDaManha = "sim" where idQuarto = 1;
+update quartos set foto = "https://imagens-revista.vivadecora.com.br/uploads/2019/05/quarto-de-hotel-decorado-em-cores-neutras-com-amplas-janelas.jpg" where idQuarto = 1;
+
+select * from quartos;
+
+
+insert into quartos (andar, numeroQuarto, tipoQuarto, ocupacaoMax, situacao, nome, descricao, foto, preco, cafeDaManha, tipoCama, varanda) values
+("3º", "302", "Luxo", 2, "sim", "Casal", "O quarto de 23m² com piso frio, sacada com belíssima vista mar. Oferece ar condicionado individual, TV LCD 42’’, wi-fi grátis,
+cofre digital, frigobar abastecido, banheiro com secador de cabelo, amenities e mesa de trabalho.",
+"https://images.trvl-media.com/lodging/1000000/40000/34900/34877/7ccc8057.jpg?impolicy=resizecrop&rw=500&ra=fit", 572.90, "sim", "King Size", "não");
 
 
 
+select * from quartos where situacao = "não" order by preco asc;
 
+select * from quartos where cafeDaManha = "sim" and situacao = "não";
+
+select * from quartos where varanda = "sim" and cafeDaManha = "sim" and situacao = "não";
+
+select * from quartos where preco < 700.00 and situacao = "não";
+
+
+create table clientes (
+	idCliente int primary key auto_increment,
+    nomeCompleto varchar(100) not null,
+    cpf char(14) not null unique,
+    rg char(12) not null unique,
+    email varchar(50) unique,
+    celular varchar(20) not null,
+    numeroCartao varchar(20) not null unique,
+	nomeTitular varchar(100) not null, 
+    validade date not null,
+    cvv char(3) not null,
+    checkin datetime not null,
+    checkout datetime not null,
+    idQuarto int not null,
+    foreign key (idQuarto) references quartos (idQuarto)
+);
 
 
 
